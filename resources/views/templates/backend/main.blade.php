e14f1ea70bc47884574858458b2e1e0d1236b4ba
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-06-16 14:34:49 +0700

    feat [development] : Setting backend layout templates

---
+++ resources/views/templates/backend/main.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Apps Name | @yield('name')</title>
+
+    {{-- General CSS Files --}}
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+
+    {{-- CSS Libraries --}}
+    <link rel="stylesheet" href="{{ asset('library/jqvmap/dist/jqvmap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/owl.carousel/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/owl.carousel/dist/assets/owl.theme.default.min.css') }}">
+
+    {{-- Template CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+
+    {{-- Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    
+    {{-- Wrapper : Start --}}
+    <div id="app">
+        <div class="main-wrapper">
+
+            {{-- Navbar : Start --}}
+            @include('templates.backend.fragments.navbar.main')
+            {{-- Navbar : End --}}
+
+            {{-- Sidebar : Start --}}
+            @include('templates.backend.fragments.sidebar.main')
+            {{-- Sidebar : End --}}
+    
+            {{-- Content : Start --}}
+            <div class="main-content">
+                <section class="section">
+
+                    {{-- Your Content : Start --}}
+                    @yield('content')
+                    {{-- Your Content : End --}}
+
+                </section>
+            </div>
+            {{-- Content : End --}}
+
+            {{-- Footer : Start --}}
+            @include('templates.backend.fragments.footer.main')
+            {{-- Footer : End --}}
+
+        </div>
+    </div>
+    {{-- Wrapper : End --}}
+
+    {{-- General JS Files --}}
+    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('library/popper.js/dist/popper.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/stisla.js') }}"></script>
+
+    {{-- JS Libraries --}}
+    <script src="{{ asset('library/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('library/chart.js/dist/Chart.min.js') }}"></script>
+    <script src="{{ asset('library/owl.carousel/dist/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('library/summernote/dist/summernote-bs4.min.js') }}"></script>
+    <script src="{{ asset('library/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+    {{-- Template JS --}}
+    <script src="{{ asset('assets/js/scripts.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+    {{-- Page Specific JS Files --}}
+    <script src="{{ asset('assets/js/page/index.js') }}"></script>
+
+</body>
+
+</html>
